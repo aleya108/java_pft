b3721880d36a97806d69592d0b810816caca7c1d
Юрий <aleyu@mail.ru>
2019-10-25 21:11:37 +0400

    создан проект 'sandbox'

---
+++ sandbox/src/main/java/main.java
@@ -0,0 +1,24 @@
+import sun.awt.geom.AreaOp;
+
+public class main {
+        int summ(int a, int b, int c) {
+        int result = a - b * c;
+        return result;
+    }
+}
+    class funSum {
+    public static void main(String[] args) {
+
+        main r = new main();
+        int summaInta = r.summ(10,20,25);
+        System.out.println(summaInta);
+
+
+        int summaDr = 80+99+108;
+        System.out.println(summaDr);
+
+    }
+
+
+}
+
