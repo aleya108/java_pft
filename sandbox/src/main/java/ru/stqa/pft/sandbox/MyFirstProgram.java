cd7e9935bad18c92c841d61ff45eeb318832f69b
Юрий <aleyu@mail.ru>
2019-10-26 21:18:21 +0400

    добавлены классы с методами area

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,11 +4,13 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("World");
     hello("Yana");
-    double len = 7;
-    System.out.println("Area of square with " + len + " is " + area(len));
-    double a = 5;
-    double b = 7;
-      System.out.println("Area of rectangle with " + a + " and " + b+ " is " + area(a,b));
+    Square s = new Square(6);
+
+
+    System.out.println("Area of square with " + s.l + " is " + s.area ());
+    Rectangle r = new Rectangle(6.2, 10.8);
+
+      System.out.println("Area of rectangle with " + r.a + " and " + r.b+ " is " + r.area());
 
 
   }
@@ -17,13 +19,9 @@ public class MyFirstProgram {
 
     System.out.println("Hello, " + smb + "!");}
 
-    public static double area (double l) {
-      return l * l;
-    }
 
-    public static double area (double a, double b) {
-      return a*b;
-    }
+
+
 
 
 }
